a8e8aefa64ced4e406d7bd2d6c3d64744c21d33d
ErkutAkın <erkutaknn@gmail.com>
2023-10-23 19:23:33 +0300

    ErkutTatilSitesi

---
+++ AkınHotelProje/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AkınHotelProje.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
