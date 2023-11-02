fe0f12bb72d15bf9d52f0c14ad0d8c547cb7a924
erkutakn <erkutaknn@gmail.com>
2023-11-02 03:10:08 +0300

    temayı Partiallara ayırdım

---
+++ AkınHotelProje/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkınTurizmProje.ViewComponents.Default
+{
+    public class _PopularDestinations:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ AkınHotelProje/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkınTurizmProje.ViewComponents.Default
+{
+    public class _SliderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
