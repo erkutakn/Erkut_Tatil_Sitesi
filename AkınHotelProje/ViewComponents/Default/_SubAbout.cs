0ea73ad645c13bead3b03a0a6da425e302413b53
ErkutAkın <erkutaknn@gmail.com>
2023-11-20 14:29:45 +0300

    güncelleme

---
+++ AkınHotelProje/ViewComponents/Default/_SubAbout.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkınTurizmProje.ViewComponents.Default
+{
+    public class _SubAbout : ViewComponent
+    {
+        SubAboutManager subAboutManager = new SubAboutManager(new EfSubAboutDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = subAboutManager.TGetList();
+            return View(values);
+
+        }
+    }
+}
