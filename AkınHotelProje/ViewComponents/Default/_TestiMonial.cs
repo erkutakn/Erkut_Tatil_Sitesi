3461da091c4abd8d02ccb6799a7634e43a7fb7ac
ErkutAkın <erkutaknn@gmail.com>
2023-11-20 18:18:55 +0300

    güncelleme

---
+++ AkınHotelProje/ViewComponents/Default/_TestiMonial.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkınTurizmProje.ViewComponents.Default
+{
+    public class _TestiMonial : ViewComponent
+    {
+        TestiMonialManager testiMonialManager = new TestiMonialManager(new EfTestiMonialDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = testiMonialManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/TestiMonialManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class TestiMonialManager:ITestiMonialService
+    {
+        ITestiMonialDal _testiMonialDal;
+
+        public TestiMonialManager(ITestiMonialDal testiMonialDal)
+        {
+            _testiMonialDal = testiMonialDal;
+        }
+
+        public void TAdd(TestiMonial t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(TestiMonial t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TestiMonial TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TestiMonial> TGetList()
+        {
+            return _testiMonialDal.GetList();
+        }
+
+        public void TUpdate(TestiMonial t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
